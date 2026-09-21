1ee2c5a461862003b2c5327d9d77a7be8ef49f01
Shivam Maurya <shivammaurya1611@gmail.com>
2026-09-21 13:45:40 +0530

    tools created

---
+++ main.py
@@ -0,0 +1,11 @@
+from src.tools.tools import web_search, scrape_url
+
+# result = web_search.invoke({
+#     "query": "latest advancements in AI technology"
+# })
+
+# print(result)
+
+# result = scrape_url("https://innowise.com/blog/ai-trends")
+
+# print(result)
